ca122974c5e7cea32c651693301a6c6596408306
sikder999 <mohsinsikder999@gmail.com>
2022-11-05 01:48:29 +0600

    mohsin1

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { Head, Link } from "@inertiajs/inertia-vue3";
+import Frontend from "@/Layouts/Frontend.vue";
+
+</script>
+
+<template>
+  <Head title="Welcome to portfolio" />
+  <Frontend>
+  </Frontend>
+</template>
